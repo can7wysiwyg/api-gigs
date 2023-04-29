618333cb93e8e3e4a55cbd6396c26b5f28eeb7b1
can7wysiwyg <paulkssa@gmail.com>
2023-04-29 09:33:09 +0200

    password reset

--- routes/Auth.js
+++ routes/Auth.js
@@ -108,6 +108,56 @@ Auth.post("/auth/login", asyncHandler(async(req, res) => {
 }))
 
 
+Auth.post(
+  "/auth/forgot_password",
+  asyncHandler(async (req, res) => {
+    const { securityAnswer, email } = req.body;
+
+    if (!securityAnswer || !email) {
+      res.json({ msg: "fields cannot be empty." });
+    }
+
+    const emailFound = await User.findOne({ email });
+    const answerFound = await User.findOne({ securityAnswer });
+
+  
+
+
+    if (emailFound && answerFound) {
+      const accessToken =  createAccessToken( { id: emailFound._id })
+      
+      res.json({ accessToken });
+    } else {
+      res.json({ msg: "please contact the admin to help you in password reset." });
+    }
+  })
+);
+
+Auth.put(
+  "/auth/reset_password",
+  verify,
+  asyncHandler(async (req, res) => {
+    const { password } = req.body;
+
+    if (!password) {
+      res.json({ msg: "field cannot be empty." });
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    await User.findOneAndUpdate(
+      { _id: req.user.id },
+      {
+        password: passwordHash,
+      }
+    );
+
+    res.json({ msg: "succesfully updated" });
+  })
+);
+
+
+
 
 const createAccessToken = (user) =>{
     return jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '7d'})
