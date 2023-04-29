1b9966fa49d19abf79f0e71a511218db06726aa6
can7wysiwyg <paulkssa@gmail.com>
2023-04-29 11:03:43 +0200

    i was creating models

---
+++ models/SubjectModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const SubjectSchema = mongoose.Schema({
+
+    subjectOwner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    subjectName: {
+        type: String,
+        required: true
+    },
+    subjectPrice: {
+        type: String,
+        required: true
+    }
+
+
+}, {
+    timestamps: true
+})
+
+
+module.exports = mongoose.model('Subject', SubjectSchema)
---
+++ models/TutorQualificationModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const QualificationSchema = mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    qualification: {
+        type: String
+    },
+    tutorSpecialty1: {
+        type: String,
+        required: true
+    },
+    tutorSpecialty2: {
+        type: String
+
+    },
+    tutorSpecialty3: {
+        type: String
+
+    }
+
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("TQualification", QualificationSchema)
